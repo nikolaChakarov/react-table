53e89bda95580ddd7081fc4d3729ef4d236f8d36
Kiril Mavrov <kiril.mavrov@digitall.com>
2023-05-09 17:45:42 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import BasicTable from './components/basic-table';
+
+const App = () => {
+  return <div>
+    <BasicTable />
+  </div>
+}
+
+export default App;
